2347be7650a9e80280480889b64eafae3728bd5e
Molly <qc8659ba@go.minneapolis.edu>
2019-04-18 16:58:07 -0500

    4/18/19

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,68 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Question
+            v-for="question in questions"
+            v-bind:key="question.question"
+            v-bind:question="question"
+            v-on:answer="addToList"
+    ></Question>
+
+    <h1>You Would Rather...</h1>
+      <ul>
+          <li v-on:>{{questions[0].uAns}}</li>
+          <li v-on:>{{questions[1].uAns}}</li>
+          <li v-on:>{{questions[2].uAns}}</li>
+      </ul>
+<!--    <Choices v-bind:choice="choice"></Choices>-->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Question from "./components/Question";
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Question,
+
+  },
+  data(){
+    return{
+        answer: '',
+      questions:[
+        {
+          id: 0,
+          question: 'be able to see things that are very far away,' +
+                  ' like binoculars or be able to see things very close up,' +
+                  ' like a microscope?',
+          answer1: 'Binocular eyes',
+          answer2: 'Scope eyes',
+          uAns: ''
+        },
+        {
+          id: 1,
+          question: 'everything in your house be one color or' +
+                  'every room be a different color?',
+          answer1: 'Whole house',
+          answer2: 'Only rooms',
+          uAns: ''
+        },
+        {
+          id: 2,
+          question: 'be able to control fire or water?',
+          answer1: 'Fire',
+          answer2: 'Water',
+          uAns: ''
+        }
+      ],
+    }
+  },
+  methods:{
+      addToList(anser, id){
+          this.uAns = anser;
+          this.questions[id].uAns.push(this.uAns);
+      }
   }
 }
 </script>
---
+++ src/components/Question.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="hello">
+        <h1>"Would you rather..."</h1>
+
+        <p>
+            {{question.question}}
+        </p>
+        <input type="radio" name="answer1" v-model="change" v-on:change="this.$emit('answer', change, questions.id)">{{question.answer1}}
+        <input type="radio" name="answer2" v-model="change" v-on:change="this.$emit('answer', change, questions.id)">{{question.answer2}}
+
+    </div>
+
+
+
+</template>
+
+<script>
+
+    export default {
+        name: "Question",
+        props:{
+            question: Object
+        },
+        data(){
+            return{
+                change: ''
+            }
+        },
+        methods:{
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
